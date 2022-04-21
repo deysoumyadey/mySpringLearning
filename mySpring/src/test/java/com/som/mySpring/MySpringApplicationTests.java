cfe556c1bfc6ad9664c01f46b23d6f92ec4ad8b3
deysoumyadey <dey.soumyadey@gmail.com>
2022-04-21 13:19:50 -0400

    initial commit

---
+++ mySpring/src/test/java/com/som/mySpring/MySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.som.mySpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
